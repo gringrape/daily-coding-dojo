9257330a1a7a454eed526e74cb7b839e5a87b28d
gringrape <53764714+gringrape@users.noreply.github.com>
2020-12-18 12:24:40 +0900

    예산

--- 20201218/budget.test.js
+++ 20201218/budget.test.js
@@ -1,5 +1,17 @@
+function findCount(d, budget, count = 0) {
+  const currentBudget = budget - d[0];
+  if (currentBudget < 0 || d.length === 0) {
+    return count;
+  }
+
+  return findCount(d.slice(1), currentBudget, count + 1);
+}
+
+function solution(d, budget) {
+  return findCount(d.sort((a, b) => a - b), budget);
+}
 
 test('solution', () => {
   expect(solution([1, 3, 2, 5, 4], 9)).toBe(3);
-  expect(solution([1, 3, 2, 5, 4], 9)).toBe(3);
+  expect(solution([2, 2, 3, 3], 10)).toBe(4);
 });
