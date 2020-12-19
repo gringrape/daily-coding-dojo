a52c6b04bad4a73cbf32f46b92ef1a3a3e6ea8f2
gringrape <53764714+gringrape@users.noreply.github.com>
2020-12-19 22:13:19 +0900

    큰수만들기 - 스택

---
+++ 20201219/makeBigNumber_stack.test.js
@@ -0,0 +1,38 @@
+function solution(n, k, stack = [], p = 0) {
+  if (k === 0) {
+    return stack.join('') + n.slice(p);
+  }
+
+  if (p > n.length - 1) {
+    return stack.join('').slice(0, -k);
+  }
+
+  const currentNumber = n[p];
+  if (stack.length === 0) {
+    stack.push(currentNumber);
+    return solution(n, k, stack, p + 1);
+  }
+
+  const lastIndex = stack.length - 1;
+
+  if (stack[lastIndex] < currentNumber) {
+    stack.pop();
+    return solution(n, k - 1, stack, p);
+  }
+
+  stack.push(currentNumber);
+  return solution(n, k, stack, p + 1);
+}
+
+test('solution', () => {
+  expect(solution('1924', 2)).toEqual('94');
+  expect(solution('1231234', 3)).toEqual('3234');
+  expect(solution('4177252841', 4)).toEqual('775841');
+  expect(solution('2222', 2)).toEqual('22');
+  expect(solution('11', 1)).toEqual('1');
+  expect(solution('10', 1)).toEqual('1');
+  expect(solution('10000', 2)).toEqual('100');
+  expect(solution('99998', 3)).toEqual('99');
+  expect(solution('999999', 3)).toEqual('999');
+  expect(solution('1001', 2)).toEqual('11');
+});
