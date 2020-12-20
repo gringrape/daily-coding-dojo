f5648fe5086a1a4f3b55eb2fef9065611bbf8395
gringrape <53764714+gringrape@users.noreply.github.com>
2020-12-21 04:36:11 +0900

    구명보트  정확성 통과

--- 20201221/lifeboat.test.js
+++ 20201221/lifeboat.test.js
@@ -4,10 +4,36 @@ function solution(
   sorted = people.sort((a, b) => a - b),
   count = 0,
 ) {
+  if (sorted.length === 0) {
+    return count;
+  }
+
+  const [first] = sorted;
+
+  const secondIndex = sorted.findIndex((weight) => weight > limit - first) - 1;
+
+  if (secondIndex > 0) {
+    return solution(people, limit, [
+      ...sorted.slice(1, secondIndex),
+      ...sorted.slice(secondIndex + 1),
+    ], count + 1);
+  }
+
+  if (secondIndex === -2) {
+    return solution(people,
+      limit,
+      sorted.slice(1, -1), count + 1);
+  }
+
+  return solution(people, limit, sorted.slice(1), count + 1);
 }
 
 test('solution', () => {
-
-
+  expect(solution([], 100)).toBe(0);
+  expect(solution([100], 100)).toBe(1);
+  expect(solution([100, 100], 100)).toBe(2);
+  expect(solution([40, 40], 100)).toBe(1);
+  expect(solution([70, 80, 50], 100)).toBe(3);
+  expect(solution([40, 40, 80, 80], 140)).toBe(2);
   // expect(solution([70, 50, 80, 50])).toBe(3);
 });
