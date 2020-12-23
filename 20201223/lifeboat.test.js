5cebcdd1470be99d65eef60cd4ec20c498e60767
gringrape <53764714+gringrape@users.noreply.github.com>
2020-12-23 10:37:52 +0900

    구명보트

---
+++ 20201223/lifeboat.test.js
@@ -0,0 +1,25 @@
+function solution(people, limit) {
+  people.sort((a, b) => b - a);
+  let count = 0;
+  while (people.length) {
+    if (people[0] + people[people.length - 1] <= limit) {
+      people.shift();
+      people.pop();
+    } else {
+      people.shift();
+    }
+    count++;
+  }
+  return count;
+}
+
+test('solution', () => {
+  expect(solution([100], 120)).toBe(1);
+  expect(solution([100, 100], 100)).toBe(2);
+  expect(solution([40, 40], 100)).toBe(1);
+  expect(solution([70, 80, 50], 100)).toBe(3);
+  expect(solution([40, 60, 80, 80], 140)).toBe(2);
+  expect(solution([70, 50, 80, 50], 100)).toBe(3);
+  expect(solution([70, 50, 80, 50, 60], 100)).toBe(4);
+  expect(solution([50, 70, 80, 50, 60], 80)).toBe(5);
+});
