e259c1c0cefde256f5fd57ff08cfdc4872fbb72b
gringrape <gringrape200@gmail.com>
2021-05-07 08:22:43 +0900

    음양 더하기

--- 20210507/scala/src/test/scala/Test.scala
+++ 20210507/scala/src/test/scala/Test.scala
@@ -1,7 +1,22 @@
 import org.scalatest.funsuite.AnyFunSuite
 
 class Test extends AnyFunSuite {
-  test("plus") {
-    assert((1 + 1) == 2)
+  def sumOf(absolutes: List[Int], signs: List[Boolean]) = {
+    absolutes
+      .zip(signs)
+      .map({ case (absolute, sign) =>
+        if (sign) absolute.unary_+ else absolute.unary_-
+      })
+      .sum
+  }
+
+  test("sumOf with plus signs") {
+    assert(sumOf(List(1), List(true)) == 1)
+    assert(sumOf(List(1, 2), List(true, true)) == 3)
+  }
+
+  test("sumOf with minus signs") {
+    assert(sumOf(List(1), List(false)) == -1)
+    assert(sumOf(List(1, 3), List(false, false)) == -4)
   }
 }
