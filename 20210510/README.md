ad5ba5826d908d205e8fc6abc219733c7aa2e6c8
gringrape <gringrape200@gmail.com>
2021-05-10 22:14:49 +0900

    배달

---
+++ 20210510/README.md
@@ -0,0 +1,12 @@
+# 배달
+https://programmers.co.kr/learn/courses/30/lessons/12978
+## 구하는 것
+1번 마을에 있는 음식점이 음식 주문을 받을 수 있는 마을의 개수
+## 주어진 것
+마을의 개수 N
+각 마을을 연결하는 도로 정보 road
+음식 배달이 가능한 시간 K
+
+## 지식
+### Dijkstra's algorithm
+두 꼭지점 간에 가장 짧은 경로를 찾는 알고리즘. 
--- 20210510/solution.test.js
+++ 20210510/solution.test.js
@@ -1,3 +1,38 @@
-test('simple', () => {
-  expect(1 + 1).toBe(2);
+function initializeDistance(n) {
+  return [
+    0,
+    ...Array(n - 1).fill(Number.MAX_SAFE_INTEGER),
+  ];
+}
+
+function solution() {
+  const times = [
+    [1, 0], [2, 1], [3, 4], [4, 2], [5, 3],
+  ];
+
+  return times
+    .filter(([, time]) => time <= 3)
+    .map(([no]) => no)
+    .length;
+}
+
+test('initialize distances', () => {
+  expect(initializeDistance(5)).toEqual([
+    0,
+    Number.MAX_SAFE_INTEGER,
+    Number.MAX_SAFE_INTEGER,
+    Number.MAX_SAFE_INTEGER,
+    Number.MAX_SAFE_INTEGER,
+  ]);
+});
+
+test('example', () => {
+  expect(solution(5, [
+    [1, 2, 1],
+    [2, 3, 3],
+    [5, 2, 2],
+    [1, 4, 2],
+    [5, 3, 1],
+    [5, 4, 2],
+  ], 3)).toBe(4);
 });
