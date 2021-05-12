5927df800f2133f1f6bbe053e308d60ed267b2c7
gringrape <gringrape200@gmail.com>
2021-05-13 08:15:00 +0900

    [WIP] 실패율 Java

---
+++ 20210513/java/README.md
@@ -0,0 +1,17 @@
+# 실패율
+https://programmers.co.kr/learn/courses/30/lessons/42889
+## 구하는 것
+실패율이 들어있는 배열 (내림차순)
+## 주어진 것
+- 전체 스테이지 개수
+- 사용자가 현재 멈춰있는 스테이지 배열
+## 조건
+### 각 스테이지별 실패율
+실패율 = (실 유저 수) / (도달 유저 수)
+## 배운것
+- Comparator
+    - rambda expression 을 사용해서 comparator 만들기.
+    - Collections, Integer 등에서 comparator 를 지원한다. :(
+- IntStream 을 통해 Stream<T> 의 메서드를 활용하기 위해서는 boxed 라는 메서드를 끼워넣을
+필요가 있다. 자바!! XXXXXX
+
--- 20210513/java/app/src/test/java/com/gringrape/codelife/AppTest.java
+++ 20210513/java/app/src/test/java/com/gringrape/codelife/AppTest.java
@@ -4,10 +4,33 @@
 package com.gringrape.codelife;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.stream.IntStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
-        assertEquals(1 + 1, 2);
+    int[] solution(int N, int[] stages) {
+        double[] failureRates = new double[]{0.5, 1};
+
+        return IntStream.range(1, N + 1)
+                .boxed()
+                .sorted((a, b) -> Double.compare(failureRates[b - 1], failureRates[a - 1]))
+                .mapToInt(i -> i)
+                .toArray();
+    }
+
+    @Test void simple() {
+        assertArrayEquals(
+                    solution(2, new int[]{1, 2}),
+                    new int[]{2, 1}
+                );
+
+//        assertArrayEquals(
+//                solution(5, new int[]{2, 1, 2, 6, 2, 4, 3, 3}),
+//                new int[]{3, 4, 2, 1, 5}
+//        );
     }
 }
