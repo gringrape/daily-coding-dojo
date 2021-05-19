a60f5c9e78f6472ea82af2c46736c5a4e5a37483
gringrape <gringrape200@gmail.com>
2021-05-20 00:38:52 +0900

    약수의 개수와 덧셈 - scala

--- 20210519/scala/tdd-exercise/src/test/scala/Test.scala
+++ 20210519/scala/tdd-exercise/src/test/scala/Test.scala
@@ -1,7 +1,26 @@
 import org.scalatest.funsuite.AnyFunSuite;
 
 class Test extends AnyFunSuite {
+  def solution(left: Int, right: Int): Int = {
+    def isInt(n: Double) = n.floor == n
+
+    def hasOddDivisors(n: Int) = isInt(Math.sqrt(n))
+
+    def sign(n: Int) = if (hasOddDivisors(n)) -1 else +1
+
+    Range
+      .inclusive(left, right, 1)
+      .map(n => sign(n) * n)
+      .sum
+  }
+
   test("test is working") {
     assert(1 + 1 == 2)
+  }
+
+  test("simple") {
+    assert(solution(1, 1) == -1)
+    assert(solution(1, 2) == 1)
+    assert(solution(1, 3) == 4)
   }
 }
