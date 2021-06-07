4eca64c9e396eaa7750accbb4b517e0223630df0
gringrape <gringrape200@gmail.com>
2021-06-07 16:48:23 +0900

    주식가격 - javascript
    프로그래머스: https://programmers.co.kr/learn/courses/30/lessons/42584

---
+++ 20210607/java2/README.md
@@ -0,0 +1,6 @@
+# 주식 가격
+https://programmers.co.kr/learn/courses/30/lessons/42584
+## 구하는 것
+가격이 떨어지지 않은 기간의 배열
+## 주어진 것
+초단위로 기록된 주식 가격
--- 20210607/java2/app/src/test/java/com/gringrape/stockprice/AppTest.java
+++ 20210607/java2/app/src/test/java/com/gringrape/stockprice/AppTest.java
@@ -5,10 +5,31 @@ package com.gringrape.stockprice;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class AppTest {
     @Test void testIsWorking() {
         assertThat(1 + 1).isEqualTo(2);
+    }
+
+    int[] solution(int[] prices) {
+        return IntStream.range(0, prices.length)
+                .map(i ->
+                        {
+                            var count = (int) Arrays.stream(prices, i + 1, prices.length)
+                                    .takeWhile(price -> price >= prices[i])
+                                    .count();
+                            return count == prices.length - i - 1 ? count : count + 1;
+                        })
+                .toArray();
+    }
+
+    @Test void example() {
+        assertThat(solution(new int[]{1, 2, 3, 2, 3})).isEqualTo(
+                new int[]{4, 3, 1, 1, 0}
+        );
     }
 }
