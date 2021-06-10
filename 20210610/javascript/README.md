6a9689396f49e89cd753658f9a8f0cc98404c51f
gringrape <gringrape200@gmail.com>
2021-06-11 05:12:42 +0900

    올바른 괄호 - javascript

---
+++ 20210610/javascript/README.md
@@ -0,0 +1,9 @@
+# 올바른 괄호
+https://programmers.co.kr/learn/courses/30/lessons/12909?language=javascript
+## 구하는 것
+주어진 문자열이 올바른 괄호 인지 여부
+## 주어진 것
+'(' 또는 ')' 로만 이루어진 문자열
+## 조건
+### 바르게 짝지어진 괄호
+'(' 로 열리면 반드시 ')' 로 닫혀야 함
--- 20210610/javascript/solution.test.js
+++ 20210610/javascript/solution.test.js
@@ -1,3 +1,57 @@
+class BracketStack {
+  constructor() {
+    this.data = [];
+  }
+
+  push(bracket) {
+    const last = this.data[this.data.length - 1];
+
+    if (bracket === ')' && last === '(') {
+      this.data.pop();
+      return;
+    }
+    this.data.push(bracket);
+  }
+
+  isEmpty() {
+    return !this.data.length;
+  }
+}
+
+describe('BracketStack', () => {
+  describe('push', () => {
+    it('pushes bracket', () => {
+      const stack = new BracketStack();
+
+      stack.push('(');
+
+      expect(stack.isEmpty()).toBeFalsy();
+
+      stack.push(')');
+
+      expect(stack.isEmpty()).toBeTruthy();
+    });
+  });
+});
+
+function solution(s) {
+  const stack = new BracketStack();
+
+  [...s].forEach((bracket) => {
+    stack.push(bracket);
+  });
+
+  return stack.isEmpty();
+}
+
 test('simple', () => {
-  expect(1 + 1).toBe(2);
+  expect(solution('()')).toBeTruthy();
+});
+
+test('samples', () => {
+  expect(solution('()()')).toBeTruthy();
+  expect(solution('(())()')).toBeTruthy();
+
+  expect(solution(')()(')).toBeFalsy();
+  expect(solution('(()(')).toBeFalsy();
 });
