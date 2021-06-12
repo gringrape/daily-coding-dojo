a3c43e194d3c1bb28679af1534ebee67fa64650f
gringrape <gringrape200@gmail.com>
2021-06-12 19:01:03 +0900

    폰켓몬 - python

---
+++ 20210612/python/README.md
@@ -0,0 +1,8 @@
+# 폰켓몬
+https://programmers.co.kr/learn/courses/30/lessons/1845
+## 구하는 것
+- N / 2 마리의 폰켓몬을 선택하는 방법 중,
+가장 많은 종류의 폰켓몬을 선택하는 방법.
+## 주어진 것
+- N 마리의 폰켓몬의 종류가 담긴 배열 nums
+
--- 20210612/python/test_solution.py
+++ 20210612/python/test_solution.py
@@ -1,3 +1,12 @@
 def test_is_working():
   assert 1 + 1 == 2
-  
+
+def solution(nums):
+  n_2 = len(nums) / 2
+  kinds = len(set(nums))
+  return n_2 if kinds >= n_2 else kinds
+
+def test_samples():
+  assert solution([3, 1, 2, 3]) == 2
+  assert solution([3, 3, 3, 2, 2, 4]) == 3
+  assert solution([3, 3, 3, 2, 2, 2]) == 2
