22861cd91cc4b89933e3719dc1d71f36f3fdf2a5
gringrape <gringrape200@gmail.com>
2021-06-17 10:41:29 +0900

    프로그래머스 로또의 최고 순위와 최저 순위

--- 20210617/javascript2/solution.test.js
+++ 20210617/javascript2/solution.test.js
@@ -1,3 +1,36 @@
 test('test is working', () => {
   expect(1 + 1).toBe(2);
 });
+
+function rankOf(count) {
+  if (count < 2) {
+    return 6;
+  }
+
+  return 7 - count;
+}
+
+function solution(lottos, winNums) {
+  const winCount = lottos.filter((n) => winNums.includes(n)).length;
+  const zeroCount = lottos.filter((n) => n === 0).length;
+
+  return [winCount + zeroCount, winCount]
+    .map(rankOf);
+}
+
+test('solution', () => {
+  expect(solution(
+    [44, 1, 0, 0, 31, 25],
+    [31, 10, 45, 1, 6, 19],
+  )).toEqual([3, 5]);
+
+  expect(solution(
+    [0, 0, 0, 0, 0, 0],
+    [38, 19, 20, 40, 15, 25],
+  )).toEqual([1, 6]);
+
+  expect(solution(
+    [45, 4, 35, 20, 3, 9],
+    [20, 9, 3, 45, 4, 35],
+  )).toEqual([1, 1]);
+});
