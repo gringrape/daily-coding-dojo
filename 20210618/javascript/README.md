c71af24c5b3b00be5404b0a6d4748af27b386138
gringrape <gringrape200@gmail.com>
2021-06-18 10:47:11 +0900

    신규 아이디 추천

---
+++ 20210618/javascript/README.md
@@ -0,0 +1,9 @@
+# 신규 아이디 추천
+https://programmers.co.kr/learn/courses/30/lessons/72410
+## 구하는 것
+추천 아이디
+## 주어진 것
+신규 유저가 입력한 아이디
+## 배운것
+- 정규표현식 or => |
+- 정규표현식 빈문자열 대응하기 https://stackoverflow.com/questions/19127384/what-is-a-regex-to-match-only-an-empty-string
--- 20210618/javascript/solution.test.js
+++ 20210618/javascript/solution.test.js
@@ -1,3 +1,49 @@
 test('test is working', () => {
   expect(1 + 1).toBe(2);
 });
+
+function solution(newId) {
+  const result = newId
+    .toLowerCase()
+    .replace(/[^0-9a-z-_.]/g, '')
+    .replace(/\.{2,}/g, '.')
+    .replace(/^\.|\.$/, '')
+    .replace(/^$/, 'a')
+    .slice(0, 15)
+    .replace(/\.$/, '');
+
+  return result
+    .padEnd(3, result[result.length - 1]);
+}
+
+test('solution', () => {
+  // # step1.
+  // expect(solution('...!@BaT#*..y.abcdefghijklm'))
+  //   .toBe('...!@bat#*..y.abcdefghijklm');
+
+  // # step2.
+  // expect(solution('...!@BaT#*..y.abcdefghijklm'))
+  //   .toBe('...bat..y.abcdefghijklm');
+
+  // # step3.
+  // expect(solution('...!@BaT#*..y.abcdefghijklm'))
+  //   .toBe('.bat.y.abcdefghijklm');
+
+  // # step4.
+  // expect(solution('.!@BaT#*..y.abcdefghijklm'))
+  //   .toBe('bat.y.abcdefghijklm');
+  // expect(solution('!@BaT#*..y.abcdefghijklm.'))
+  //   .toBe('bat.y.abcdefghijklm');
+
+  // # step5.
+  // expect(solution(''))
+  //   .toBe('a');
+
+  // # step6.
+  // expect(solution('bat.y.abcdefghijklm'))
+  //   .toBe('bat.y.abcdefghi');
+
+  // # step7.
+  expect(solution('=.='))
+    .toBe('aaa');
+});
