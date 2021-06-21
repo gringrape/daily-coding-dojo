2cd585d51e43191a7b81b522981d73a3f5182623
gringrape <gringrape200@gmail.com>
2021-06-22 00:14:46 +0900

    프로그래머스 단어 변환

---
+++ 20210621/javascript3/README.md
@@ -0,0 +1,11 @@
+# 단어 변환
+https://programmers.co.kr/learn/courses/30/lessons/43163
+## 구하는 것
+begin 을 target 으로 변환하기 위한 최소 단계의 수
+## 주어진 것
+- 두개의 단어 begin, target
+- 단어의 집합 words
+## 조건
+### 변환이란?
+- 한번에 한글자씩 만 바꿀수 있음.
+- words 에 있는 단어로만 변환가능
--- 20210621/javascript3/solution.test.js
+++ 20210621/javascript3/solution.test.js
@@ -1,3 +1,49 @@
+/* eslint-disable no-unused-vars */
 test('test is working', () => {
   expect(1 + 1).toBe(2);
+});
+
+function solution(begin, target, words) {
+  function countMismatch(word1, word2) {
+    return Array(word1.length).fill().map((_, i) => i)
+      .filter((i) => word1[i] !== word2[i]).length;
+  }
+
+  if (!words.some((word) => word === target)) {
+    return 0;
+  }
+
+  let step = 0;
+  const q = [];
+  q.push(begin);
+  while (q.length > 0) {
+    step += 1;
+    const visits = q.length;
+
+    for (let i = 0; i < visits; i++) {
+      const current = q.shift();
+
+      if (current === target) {
+        return step - 1;
+      }
+
+      const nextWords = words
+        .filter((word) => countMismatch(current, word) === 1);
+      nextWords.forEach((nextWord) => q.push(nextWord));
+    }
+  }
+  return 4;
+}
+
+test('samples', () => {
+  expect(solution(
+    'hit',
+    'cog',
+    ['hot', 'dot', 'dog', 'lot', 'log', 'cog'],
+  )).toBe(4);
+  expect(solution(
+    'hit',
+    'cog',
+    ['hot', 'dot', 'dog', 'lot', 'log'],
+  )).toBe(0);
 });
