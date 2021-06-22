a8303591bff9f8851bf1131a3ab39b4dd0cda8ce
gringrape <gringrape200@gmail.com>
2021-06-22 10:53:26 +0900

    프로그래머스 폰켓몬

--- 20210622/javascript2/solution.test.js
+++ 20210622/javascript2/solution.test.js
@@ -1,3 +1,18 @@
 test('test works', () => {
   expect(1 + 1).toBe(2);
 });
+
+function solution(nums) {
+  const kinds = (new Set(nums)).size;
+  const N = nums.length;
+
+  return kinds >= N / 2
+    ? N / 2
+    : kinds;
+}
+
+test('samples', () => {
+  expect(solution([3, 1, 2, 3])).toBe(2);
+  expect(solution([3, 3, 3, 2, 2, 4])).toBe(3);
+  expect(solution([3, 3, 3, 2, 2, 2])).toBe(2);
+});
