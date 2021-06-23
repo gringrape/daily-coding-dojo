7a60010623b1fb9fa9d4cbc65523271e5cd7f69e
gringrape <gringrape200@gmail.com>
2021-06-24 06:19:21 +0900

    프로그래머스 최고의집합

--- 20210624/javascript/solution.test.js
+++ 20210624/javascript/solution.test.js
@@ -1,3 +1,23 @@
 test('test works', () => {
   expect(1 + 1).toBe(2);
 });
+
+function solution(n, s) {
+  if (s < n) {
+    return [-1];
+  }
+
+  const remainder = s % n;
+  const quotient = (s - remainder) / n;
+
+  return [
+    ...Array(n - remainder).fill(quotient),
+    ...Array(remainder).fill(quotient + 1),
+  ];
+}
+
+test('solution', () => {
+  expect(solution(2, 8)).toEqual([4, 4]);
+  expect(solution(2, 9)).toEqual([4, 5]);
+  expect(solution(2, 1)).toEqual([-1]);
+});
