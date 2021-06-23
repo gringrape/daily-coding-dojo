5881c48d185d664ff3c2c84b168ea6be3096cded
gringrape <gringrape200@gmail.com>
2021-06-23 12:46:46 +0900

    프로그래머스 음양더하기

---
+++ 20210623/javascript/README.md
@@ -0,0 +1,7 @@
+# 음양 더하기
+https://programmers.co.kr/learn/courses/30/lessons/76501
+## 구하는 것
+실제 정수들의 합
+## 주어진 것
+- 절댓값들의 배열
+- 부호들의 배열
--- 20210623/javascript/solution.test.js
+++ 20210623/javascript/solution.test.js
@@ -1,3 +1,35 @@
 test('test works', () => {
   expect(1 + 1).toBe(2);
 });
+
+function zip(...arrays) {
+  const width = arrays.length;
+  const { length } = arrays[0];
+
+  return Array(length).fill()
+    .map((_, i) => Array(width).fill().reduce((a, x, j) => [...a, arrays[j][i]], []));
+}
+
+test('zip', () => {
+  expect(zip([1, 2, 3], [4, 5, 6], [7, 8, 9]))
+    .toEqual([
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 6, 9],
+    ]);
+});
+
+function solution(absolutes, signs) {
+  const signValues = signs.map((sign) => (sign ? 1 : -1));
+
+  return zip(signValues, absolutes)
+    .map(([sign, abs]) => sign * abs)
+    .reduce((a, c) => a + c, 0);
+}
+
+test('samples', () => {
+  expect(solution(
+    [4, 7, 12],
+    [true, false, true],
+  )).toBe(9);
+});
