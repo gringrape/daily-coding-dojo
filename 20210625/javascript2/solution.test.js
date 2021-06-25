ca4ed2f6b9e3f814dae20006100a600ee9e6fa26
gringrape <gringrape200@gmail.com>
2021-06-25 13:53:29 +0900

    프로그래머스 비밀지도

--- 20210625/javascript2/solution.test.js
+++ 20210625/javascript2/solution.test.js
@@ -1,3 +1,24 @@
+/* eslint-disable no-bitwise */
 test('test works', () => {
   expect(1 + 1).toBe(2);
+});
+
+function range(n) {
+  return Array(n).fill().map((_, i) => i);
+}
+
+function solution(n, arr1, arr2) {
+  return range(n)
+    .map((i) => (arr1[i] | arr2[i]).toString(2)
+      .padStart(n, '0')
+      .replace(/0/g, ' ')
+      .replace(/1/g, '#'));
+}
+
+test('samples', () => {
+  expect(solution(
+    5,
+    [9, 20, 28, 18, 11],
+    [30, 1, 21, 17, 28],
+  )).toEqual(['#####', '# # #', '### #', '#  ##', '#####']);
 });
