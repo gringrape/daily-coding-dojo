33b7039d8966d65b9b975f0aa07f38e846a671e8
gringrape <gringrape200@gmail.com>
2021-06-30 13:42:46 +0900

    프로그래머스 - 가장 긴 팰린드롬

---
+++ 20210630/scala/palindrome/README.md
@@ -0,0 +1,2 @@
+# 가장 긴 팰린드롬
+https://programmers.co.kr/learn/courses/30/lessons/12904
--- 20210630/scala/palindrome/src/test/scala/Test.scala
+++ 20210630/scala/palindrome/src/test/scala/Test.scala
@@ -1,7 +1,42 @@
 import org.scalatest.funsuite.AnyFunSuite
+import scala.annotation.tailrec
 
 class Test extends AnyFunSuite {
+  def solution(s: String): Int = {
+    var results = List[Int]()
+
+    def go(start: Int = 0, end: Int = s.length() - 1, count: Int = 0): Unit = {
+      if (start > end) {
+        results = count :: results
+        return
+      }
+
+      if (start == end) {
+        results = (count + 1) :: results
+        return
+      }
+
+      if (s.charAt(start) == s.charAt(end)) {
+        go(start + 1, end - 1, count + 2)
+        return
+      }
+
+      go(start + 1, end)
+      go(start, end - 1)
+      go(start + 1, end - 1)
+    }
+
+    go()
+
+    results.max
+  }
+
   test("test works") {
     assert(1 + 1 === 2)
+  }
+
+  test("sample") {
+    assert(solution("abcdcba") == 7)
+    assert(solution("abacde") == 3)
   }
 }
