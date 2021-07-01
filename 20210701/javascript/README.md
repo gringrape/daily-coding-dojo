6c675409deb45c60f10c27eace69585a6dc1933c
gringrape <gringrape200@gmail.com>
2021-07-01 10:50:13 +0900

    프로그래머스 - 3 진법 뒤집기

---
+++ 20210701/javascript/README.md
@@ -0,0 +1,6 @@
+# 3 진법 뒤집기
+https://programmers.co.kr/learn/courses/30/lessons/68935
+## 구하는 것
+n 을 3 진법 상에서 앞뒤로 뒤집은 후, 이를 다시 10진법으로 표현한 수 
+## 주어진 것
+자연수 n
--- 20210701/javascript/solution.test.js
+++ 20210701/javascript/solution.test.js
@@ -1,3 +1,14 @@
 test('test works', () => {
   expect(1 + 1).toBe(2);
 });
+
+function solution(n) {
+  return parseInt(
+    n.toString(3).split('').reverse().join(''),
+    3,
+  );
+}
+
+test('samples', () => {
+  expect(solution(45)).toBe(7);
+});
