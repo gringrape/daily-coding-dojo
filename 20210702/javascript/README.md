3e9a44474105108344ebdaa75c678c59b591234b
gringrape <gringrape200@gmail.com>
2021-07-02 12:05:14 +0900

    프로그래머스 - 가장 큰 수

---
+++ 20210702/javascript/README.md
@@ -0,0 +1,6 @@
+# 가장 큰 수
+https://programmers.co.kr/learn/courses/30/lessons/42746
+## 구하는 것
+순서를 재배치하여 만들 수 있는 가장 큰 수 -> 문자열
+## 주어진 것
+0 또는 양의 정수 들이 담긴 배열
--- 20210702/javascript/solution.test.js
+++ 20210702/javascript/solution.test.js
@@ -1,3 +1,23 @@
 test('test works', () => {
   expect(1 + 1).toBe(2);
 });
+
+function solution(numbers) {
+  if (Math.max(...numbers) === 0) {
+    return '0';
+  }
+
+  return numbers
+    .map((number) => number.toString())
+    .sort((a, b) => (a + b > b + a ? -1 : 1))
+    .join('');
+}
+
+test('0', () => {
+  expect(solution([0, 0, 0])).toBe('0');
+});
+
+test('sample', () => {
+  expect(solution([6, 10, 2])).toBe('6210');
+  expect(solution([3, 30, 34, 5, 9])).toBe('9534330');
+});
