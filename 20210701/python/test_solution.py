b082cbfa6d5e4167f58090fb1051f7bbea05a0b6
gringrape <gringrape200@gmail.com>
2021-07-02 11:31:18 +0900

    프로그래머스 - 후보키
    리팩토링

--- 20210701/python/test_solution.py
+++ 20210701/python/test_solution.py
@@ -5,27 +5,48 @@ from itertools import combinations
 
 def solution(relation):
   def is_unique(key):
-    columns = {'-'.join([row[c] for c in key]) for row in relation}
+    hashes = [
+      '-'.join(row[i] for i in key)
+      for row in relation
+    ]
 
-    return len(relation) == len(columns)
+    return len(set(hashes)) == len(relation)
 
-  indexes = [i for i in range(len(relation[0]))]
-  keys = [comb for i in range(len(relation)) for comb in combinations(indexes, i) ]
-  
-  unique_keys = [
-    key 
-    for key in keys
-    if is_unique(key)
+  def is_minimal(key, unique_keys):
+    other_keys = (other_key for other_key in unique_keys if key is not other_key)
+    return not any(set(key).issuperset(set(other_key)) for other_key in other_keys)
+
+  indexes = [i for i in range(0, len(relation[0]))]
+  keys = [
+    combination 
+    for i in range(1, len(relation[0]) + 1)
+    for combination in combinations(indexes, i)
   ]
-
-  result_keys = [
-    key
-    for key in unique_keys
-    if not any(set(other_key).issubset(set(key)) for other_key in unique_keys if key is not other_key)
-  ]
-
-  return len(result_keys)
   
+  unique_keys = [key for key in keys if is_unique(key)]
+  minimal_unique_keys = [key for key in unique_keys if is_minimal(key, unique_keys)]
+
+  return len(minimal_unique_keys)
+
+def test_simple():
+  assert solution([
+    ['100'],
+    ['200']
+  ]) == 1
+
+def test_unique():
+  assert solution([
+    ['100', 'ryan'],
+    ['200', 'ryan']
+  ]) == 1
+
+def test_combination_of_keys():
+  assert solution([
+    ['100', 'ryan'],
+    ['200', 'ryan'],
+    ['200', 'angel'],
+  ]) == 1
+
 def test_samples():
   assert solution(
     [
