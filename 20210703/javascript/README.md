4e8e09050a119d79a0cf2385a0ea396c804765b2
gringrape <gringrape200@gmail.com>
2021-07-03 09:15:23 +0900

    ...asdf...

---
+++ 20210703/javascript/README.md
@@ -0,0 +1,6 @@
+# 가장 긴 팰린드롬
+https://programmers.co.kr/learn/courses/30/lessons/12904
+## 구하는 것
+부분 문자열 중 가장 긴 팰린드롬의 길이
+## 주어진 것
+문자열 s
--- 20210703/javascript/solution.test.js
+++ 20210703/javascript/solution.test.js
@@ -1,3 +1,57 @@
 test('test works', () => {
   expect(1 + 1).toBe(2);
 });
+
+function range(from, to) {
+  if (from >= to) {
+    return [];
+  }
+  return Array(to - from).fill().map((_, i) => i + from);
+}
+
+function solution(s) {
+  if (s.length === 1) {
+    return 1;
+  }
+
+  if (s.length === 2) {
+    return s[0] === s[1] ? 2 : 0;
+  }
+
+  const N = s.length;
+  const d = Array(N).fill(0)
+    .map(() => Array(N).fill(-1));
+
+  range(0, N).forEach((i) => {
+    d[i][i] = 1;
+    d[i][i + 1] = s[i] === s[i + 1] ? 2 : 0;
+  });
+
+  range(2, N).forEach((j) => {
+    range(0, N - 1).forEach((i) => {
+      if (i + j <= N - 1) {
+        d[i][i + j] = d[i + 1][i + j - 1] + (s[i] === s[i + j] ? 2 : 0);
+      }
+    });
+  });
+
+  return Math.max(...d.flat());
+}
+
+test('simple', () => {
+  expect(solution('a')).toBe(1);
+  expect(solution('ab')).toBe(0);
+  expect(solution('aa')).toBe(2);
+});
+
+test('longer than 2', () => {
+  expect(solution('aba')).toBe(3);
+  expect(solution('aaa')).toBe(3);
+  expect(solution('aab')).toBe(2);
+  expect(solution('abc')).toBe(1);
+});
+
+test('sample', () => {
+  expect(solution('abcdcba')).toBe(7);
+  expect(solution('abacde')).toBe(3);
+});
