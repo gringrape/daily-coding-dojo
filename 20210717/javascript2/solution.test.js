3b02b420736fef376b956ad3eed4df70c1b52c9d
gringrape <gringrape200@gmail.com>
2021-07-18 02:49:48 +0900

    프로그래머스 - N-Queen

--- 20210717/javascript2/solution.test.js
+++ 20210717/javascript2/solution.test.js
@@ -1,3 +1,36 @@
 test('test works', () => {
   expect(1 + 1).toBe(2);
 });
+
+function range(start, end) {
+  return [...Array(end - start)].map((_, i) => start + i);
+}
+
+function solution(n) {
+  function count({ row, path }) {
+    if (row > n - 1) {
+      return 1;
+    }
+
+    const verticalSafe = (column) => path.every(([, c]) => c !== column);
+    const diagonalSafe = (column) => path.every(([r, c]) => {
+      const [distX, distY] = [row - r, column - c].map((i) => Math.abs(i));
+      return distX !== distY;
+    });
+
+    return range(0, n)
+      .filter((column) => verticalSafe(column) && diagonalSafe(column))
+      .reduce((accumulator, column) => (
+        accumulator + count({
+          row: row + 1,
+          path: [...path, [row, column]],
+        })
+      ), 0);
+  }
+
+  return count({ row: 0, path: [] });
+}
+
+test('sample', () => {
+  expect(solution(4)).toBe(2);
+});
