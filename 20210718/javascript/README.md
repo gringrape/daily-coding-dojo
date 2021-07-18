ab813c6fbc4be3f57a775d500b5db4479f80816c
gringrape <gringrape200@gmail.com>
2021-07-19 08:56:24 +0900

    프로그래머스 - 거스름돈

---
+++ 20210718/javascript/README.md
@@ -0,0 +1,7 @@
+# 거스름돈
+https://programmers.co.kr/learn/courses/30/lessons/12907
+## 구하는 것
+n 원을 거슬러주는 방법의 수
+## 주어진 것
+- 거슬러 줘야 하는 금액 n
+- Finn 이 현재 보유하고 있는 돈의 종류
--- 20210718/javascript/solution.test.js
+++ 20210718/javascript/solution.test.js
@@ -1,3 +1,19 @@
-test('test works', () => {
-  expect(1 + 1).toBe(2);
+function solution(n, money) {
+  const dp = Array(n + 1).fill(0);
+
+  dp[0] = 1;
+  money.forEach((m) => {
+    for (let j = m; j <= n; j++) {
+      dp[j] += dp[j - m];
+    }
+  });
+
+  return dp[n];
+}
+
+test('sample', () => {
+  expect(solution(
+    5,
+    [1, 2, 5],
+  )).toBe(4);
 });
