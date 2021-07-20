672d524e5d3c0238a2639be0113f0117cfb01ac2
gringrape <gringrape200@gmail.com>
2021-07-21 03:38:29 +0900

    프로그래머스 - 가장 먼 노드

---
+++ 20210721/javascript/README.md
@@ -0,0 +1,7 @@
+# 가장 먼 노드
+https://programmers.co.kr/learn/courses/30/lessons/49189
+## 구하는 것
+1 번 노드로 부터 가장 멀리 떨어진 노드의 개수
+## 주어진 것
+- 노드의 개수 n
+- 간선 정보 edges
--- 20210721/javascript/solution.test.js
+++ 20210721/javascript/solution.test.js
@@ -1,3 +1,65 @@
-test('test works', () => {
-  expect(1 + 1).toBe(2);
+function range(start, end) {
+  return Array.from({ length: end - start }, (_, i) => start + i);
+}
+
+function connect(n, edges) {
+  const network = new Map();
+
+  range(0, n).map((i) => i + 1).forEach((i) => {
+    network.set(i, []);
+  });
+
+  edges.forEach((nodes) => {
+    const [first, second] = nodes;
+    network.get(first).push(second);
+    network.get(second).push(first);
+  });
+
+  return network;
+}
+
+test('connect', () => {
+  expect(connect(6, [
+    [3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2],
+  ]).get(3).sort()).toEqual([1, 2, 4, 6]);
+});
+
+function solution(n, edges) {
+  const network = connect(n, edges);
+  const visited = Array(n + 1).fill(0);
+  visited[1] = 1;
+
+  const Q = [];
+  Q.push([1, 1]);
+
+  let currentDistance = 1;
+  let count = 0;
+
+  while (Q.length !== 0) {
+    const [node, distance] = Q.shift();
+
+    if (currentDistance < distance) {
+      currentDistance += 1;
+      count = 1;
+    } else if (currentDistance === distance) {
+      count += 1;
+    }
+
+    const neighbors = network.get(node);
+
+    neighbors.forEach((neighbor) => {
+      if (visited[neighbor] !== 1) {
+        visited[neighbor] = 1;
+        Q.push([neighbor, distance + 1]);
+      }
+    });
+  }
+
+  return count;
+}
+
+test('sample', () => {
+  expect(solution(6, [
+    [3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2],
+  ])).toBe(3);
 });
