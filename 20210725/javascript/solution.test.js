c5e70819ca3ae06a0331ae0f19c49a37a4218bb0
gringrape <gringrape200@gmail.com>
2021-07-25 20:35:20 +0900

    프로그래머스 - 기지국 설치

--- 20210725/javascript/solution.test.js
+++ 20210725/javascript/solution.test.js
@@ -1,3 +1,26 @@
-test('test works', () => {
-  expect(1 + 1).toBe(2);
+function solution(N, stations, W) {
+  const construct = (distance) => {
+    const range = 2 * W + 1;
+    return Math.ceil(distance / range);
+  };
+
+  const intalledRanges = stations
+    .map((point) => [point - W, point + W]);
+
+  const uninstalledDistances = [[0, 0], ...intalledRanges].map((_, i, arr) => {
+    const [, currentEnd] = arr[i];
+    const [nextStart] = arr[i + 1] || [N + 1, N + 1];
+
+    return nextStart - currentEnd - 1;
+  }).filter((i) => i > 0);
+
+  return uninstalledDistances
+    .map(construct)
+    .reduce((a, c) => a + c, 0);
+}
+
+test('sample', () => {
+  expect(solution(11, [4, 11], 1)).toBe(3);
+  expect(solution(16, [9], 2)).toBe(3);
+  expect(solution(16, [1, 9], 2)).toBe(3);
 });
