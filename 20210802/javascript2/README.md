09dea844ea25b9e6975ea60aaf0a40d5ee51eaa5
gringrape <gringrape200@gmail.com>
2021-08-02 20:34:15 +0900

    프로그래머스 - 부족한 금액 계산하기

---
+++ 20210802/javascript2/README.md
@@ -0,0 +1,11 @@
+# 부족한 금액 계산하기
+https://programmers.co.kr/learn/courses/30/lessons/82612
+## 구하는 것
+모자라는 금액
+## 주어지는 것
+- 놀이기구 이용료 price
+- 소지금 money
+- 이용횟수 count
+## 조건
+### 이용료 증가
+- N 번째 이용은 N 배로 증가
--- 20210802/javascript2/solution.test.js
+++ 20210802/javascript2/solution.test.js
@@ -1,3 +1,19 @@
-test('test works', () => {
-  expect(1 + 1).toBe(2);
+function range(start, end) {
+  return [...Array(end - start + 1)]
+    .map((_, i) => i + start);
+}
+
+function solution(price, money, count) {
+  const totalPrice = range(1, count)
+    .reduce((a, i) => a + i * price, 0);
+
+  return Math.max(totalPrice - money, 0);
+}
+
+test('not enough money', () => {
+  expect(solution(3, 20, 4)).toBe(10);
+});
+
+test('enough money', () => {
+  expect(solution(3, 20, 2)).toBe(0);
 });
