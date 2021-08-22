aa90f2c67f351e5fae865fe0fc6c9fea797cc755
gringrape <gringrape200@gmail.com>
2021-08-22 21:42:46 +0900

    프로그래머스 - K 번째 수

--- 20210822/ruby/test/test_solution.rb
+++ 20210822/ruby/test/test_solution.rb
@@ -3,11 +3,14 @@ require 'minitest/reporters'
 Minitest::Reporters.use!
 
 class Test < Minitest::Test
-  def solution
-    1
+  def solution(array, commands)
+    commands.map { |i, j, k| array[i - 1..j - 1].sort[k - 1] }
   end
 
   def test_works
-    assert_equal solution, 1
+    assert_equal solution(
+      [1, 5, 2, 6, 3, 7, 4],
+      [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
+    ), [5, 6, 3]
   end
 end
