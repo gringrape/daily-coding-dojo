67ca0f7212379c3771aa76817399528e7f321f84
gringrape <gringrape200@gmail.com>
2021-08-23 07:25:54 +0900

    프로그래머스 - 타겟 넘버

---
+++ 20210823/scala/target-number/README.md
@@ -0,0 +1,13 @@
+# 타겟 넘버
+https://programmers.co.kr/learn/courses/30/lessons/43165?language=scala
+
+## 구하는 것
+'타겟 넘버'를 만드는 방법의 수
+
+## 주어진 것
+- 사용할 수 있는 숫자 배열 - numbers
+- 타겟 넘버 - target
+
+## 조건
+### 만든다
+숫자들을 적절하게 더하거나 빼서 타겟넘버를 만든다.
--- 20210823/scala/target-number/src/test/scala/Test.scala
+++ 20210823/scala/target-number/src/test/scala/Test.scala
@@ -1,7 +1,17 @@
 import org.scalatest.funsuite.AnyFunSuite
 
 class Test extends AnyFunSuite {
-  test("CubeCalculator.cube") {
-    assert(1 + 1 == 2)
+  def solution(numbers: Vector[Int], target: Int, i: Int = 0): Int = {
+    val result = if (target == 0) 1 else 0
+
+    if (i == numbers.length) result
+    else
+      solution(numbers, target + numbers(i), i + 1) +
+        solution(numbers, target - numbers(i), i + 1)
+  }
+
+  test("test simple") {
+    assert(solution(Vector(1, 1), 2) == 1)
+    assert(solution(Vector(1, 1, 1, 1, 1), 3) == 5)
   }
 }
