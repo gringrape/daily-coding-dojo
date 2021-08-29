97a43a414badbb24551beb0a2ca392d09e2ef454
gringrape <gringrape200@gmail.com>
2021-08-29 23:16:54 +0900

    프로그래머스 - x만큼 간격이 있는 n개의 숫자

---
+++ 20210829/scala/README.md
@@ -0,0 +1,7 @@
+# x만큼 간격이 있는 n개의 숫자
+https://programmers.co.kr/learn/courses/30/lessons/12954?language=scala
+
+## 구하는 것
+x 부터 시작해 x 씩 증가하는 n 개의 숫자
+## 주어진 것
+x, n
--- 20210829/scala/src/test/scala/Test.scala
+++ 20210829/scala/src/test/scala/Test.scala
@@ -1,5 +1,27 @@
 class Test extends org.scalatest.funsuite.AnyFunSuite {
-  test("CubeCalculator.cube") {
-    assert(1 + 1 === 2)
+  def solution1(x: Int, n: Int): Vector[Long] = {
+    Vector
+      .range(1, n + 1)
+      .map(_ * x)
+  }
+
+  test("solution1") {
+    assert(solution1(2, 5) == Vector(2, 4, 6, 8, 10))
+  }
+
+  def solution2(x: Int, n: Int): Vector[Long] = {
+    Vector.iterate[Long](x, n)(_ + x)
+  }
+
+  test("solution2") {
+    assert(solution2(2, 5) == Vector(2, 4, 6, 8, 10))
+  }
+
+  def solution3(x: Int, n: Int): Vector[Long] = {
+    Stream.range[Long](1, n + 1).map(_ * x).toVector
+  }
+
+  test("solution3") {
+    assert(solution3(2, 5) == Vector(2, 4, 6, 8, 10))
   }
 }
