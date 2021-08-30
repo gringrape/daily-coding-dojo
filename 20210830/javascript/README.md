ad4bb907191528cff0ef4c4619fcc6c2d3abd29a
gringrape <gringrape200@gmail.com>
2021-08-30 12:26:05 +0900

    프로그래머스 - 모음 사전

---
+++ 20210830/javascript/README.md
@@ -0,0 +1,12 @@
+# 모음사전
+https://programmers.co.kr/learn/courses/30/lessons/84512?language=javascript
+
+## 구하는 것
+단어가 사전에서 '순서'
+
+## 주어진 것
+단어 하나
+
+## 조건
+### 사전
+- 알파벳 모음 만으로 만들 수 있는, 길이 5 이하의 모든 단어 수록
--- 20210830/javascript/solution.test.js
+++ 20210830/javascript/solution.test.js
@@ -1,3 +1,37 @@
-test('test works', () => {
-  expect(1 + 1).toBe(2);
+function range(start, end) {
+  return [...Array(end - start)].map((_, i) => i + start);
+}
+
+function sum(array) {
+  return array.reduce((a, c) => a + c, 0);
+}
+
+function solution(word) {
+  const letters = 'AEIOU';
+
+  const unit = (i) => sum(
+    range(0, i).map((k) => 5 ** k),
+  );
+
+  return sum(
+    [...word].map((l, i) => (
+      letters.indexOf(l) * unit(5 - i) + 1
+    )),
+  );
+}
+
+test('a letter', () => {
+  expect(solution('A')).toBe(1);
+  expect(solution('I')).toBe(1563);
+});
+
+test('two letters', () => {
+  expect(solution('AA')).toBe(2);
+  expect(solution('IA')).toBe(1564);
+});
+
+test('sample', () => {
+  expect(solution('AAAAE')).toBe(6);
+  expect(solution('AAAE')).toBe(10);
+  expect(solution('EIO')).toBe(1189);
 });
