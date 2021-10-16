3ba93755d83402e10e37cbfcba99e9f8408f5507
gringrape <gringrape200@gmail.com>
2021-10-16 18:14:28 +0900

    프로그래머스 - 없는 숫자 더하기
    Link: https://programmers.co.kr/learn/courses/30/lessons/86051

--- 20211016/README.md
+++ 20211016/README.md
@@ -9,3 +9,27 @@ sdk install kotlin {version}
 ```bash
 gradle init
 ```
+
+# 없는 숫자 더하기 - 프로그래머스
+link: https://programmers.co.kr/learn/courses/30/lessons/86051
+
+## 구하는 것
+numbers 에 없는 수를 모두 더한 값.
+
+## 주어진 것
+0 부터 9 까지의 수 중 일부가 들어있는 배열 numbers
+
+## 조건
+numbers 에 있는 숫자들은 모두 서로 다릅니다
+
+## 계획
+0 ~ 9 까지의 수를 모두 가지고 있는 배열.
+위 배열에서 numbers 에 있는 것을 제외한다. 
+- [x] 0 ~ 9 까지의 배열
+- [x] numbers 에 포함된다?
+- [x] 포함된 것을 제외
+
+## Kotlin reference
+- range https://kotlinlang.org/docs/ranges.html#range
+- intArrayOf https://kotlinlang.org/api/latest/jvm/stdlib/kotlin/int-array-of.html
+- filterNot https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/filter-not.html
--- 20211016/app/src/test/kotlin/coding/dojo/AppTest.kt
+++ 20211016/app/src/test/kotlin/coding/dojo/AppTest.kt
@@ -3,17 +3,44 @@
  */
 package coding.dojo
 
-import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertNotNull
+import kotlin.test.*
 
 class AppTest {
-    fun sum(a: Int, b: Int): Int {
-        return a + b
+    private fun sumFromOneToFive(): Int {
+        return (1..5).sum()
     }
 
     @Test
-    fun appHasAGreeting() {
-        assertEquals(3, sum(1, 2))
+    fun testSumFromOneToFive() {
+        assertEquals(15, sumFromOneToFive())
+    }
+
+    private fun excludeFiveFromOneToFive(): Int {
+        return (1..5).filter { it != 5 }.sum()
+    }
+
+    @Test
+    fun testFilter() {
+        assertEquals(10, excludeFiveFromOneToFive())
+    }
+
+    private fun includedInOneToFive(i: Int): Boolean {
+        return (1..5).contains(i)
+    }
+
+    @Test
+    fun testInclude() {
+        assertTrue { includedInOneToFive(1) }
+        assertFalse { includedInOneToFive(6) }
+    }
+
+    private fun solution(numbers: IntArray): Int {
+        return (0..9).filterNot { numbers.contains(it) }.sum()
+    }
+
+    @Test
+    fun testSample() {
+        assertEquals(14, solution(intArrayOf(1,2,3,4,6,7,8,0)))
+        assertEquals(6, solution(intArrayOf(5,8,4,0,6,7,9)))
     }
 }
