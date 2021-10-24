722df02c2501640aca78d058060f9461f2b319b1
gringrape <gringrape200@gmail.com>
2021-10-24 19:45:01 +0900

    Customize checkbox
    Customize checkbox with styled components.

--- 20211024/react/README.md
+++ 20211024/react/README.md
@@ -1,1 +1,48 @@
 # Custom checkbox
+## styled component
+installation
+```bash
+npm install styled-components
+npm install --save-dev @types/styled-components
+```
+
+## check icon
+```html
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 0 24 24"
+	aria-hidden="true"
+	focusable="false"
+>
+	<path
+		fill="none"
+		stroke="currentColor"
+		stroke-width="3"
+		d="M1.73 12.91l6.37 6.37L22.79 4.59"
+	/>
+</svg>
+``` 
+
+## mark up
+체크박스 label, input, icon
+
+### eslint, input 과 label 연결
+`.eslintrc.js` 파일:
+```javascript
+module.exports = {
+	//...(전략)...
+	rules: {
+		'jsx-a11y/label-has-associated-control':  ['error', {
+			required: {
+				some: ['nesting', 'id'],
+			},
+		}],
+		'jsx-a11y/label-has-for': ['error', {
+			required: {
+				some: ['nesting', 'id'],
+			},
+		}],
+	},
+	//...(후략)...
+}
+```
--- 20211024/react/src/App.tsx
+++ 20211024/react/src/App.tsx
@@ -1,5 +1,82 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: min-content auto;
+  grid-gap: 10px;
+`;
+
+const InputControl = styled.span`
+  display: grid;
+  grid-template-areas: 'checkbox';
+  height: 16px;
+  width: 16px;
+`;
+
+const Input = styled.input`
+  display: inline-grid;
+  grid-area: checkbox;
+  opacity: 0;
+  z-index: 5000;
+
+  &:checked + span svg {
+    opacity: 1;
+  }
+
+  &:checked + span {
+    background-color: rgb(255, 112, 0);
+    border: 1px solid rgb(255, 112, 0);
+  }
+`;
+
+const CheckBox = styled.span`
+  display: inline-grid;
+  grid-area: checkbox;
+  border: 1px solid black;
+  border-radius: 3px;
+`;
+
+const CheckIcon = styled.svg`
+  margin: auto auto;
+  width: 12px;
+  height: 12px;
+  opacity: 0;
+  color: white;
+`;
+
+const Label = styled.span`
+  & > span {
+    height: 12px;
+    font-size: 12px;
+  }
+`;
+
 export default function App() {
   return (
-    <p>Hello, world!</p>
+    <Container>
+      <InputControl>
+        <Input type="checkbox" />
+        <CheckBox>
+          <CheckIcon
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            aria-hidden="true"
+            focusable="false"
+          >
+            <path
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="5"
+              d="M1.73 12.91l6.37 6.37L22.79 4.59"
+            />
+          </CheckIcon>
+        </CheckBox>
+      </InputControl>
+      <Label>
+        <span>
+          정말로 동의합니다
+        </span>
+      </Label>
+    </Container>
   );
 }
