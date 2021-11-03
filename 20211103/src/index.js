dd321f97e091ab11bc1e7e69f5059cb50486f351
gringrape <gringrape200@gmail.com>
2021-11-04 08:09:35 +0900

    Setup project

---
+++ 20211103/src/index.js
@@ -0,0 +1,4 @@
+export const sum = (a, b) => a + b;
+
+// TODO: delete this
+export const xx = '';
---
+++ 20211103/src/index.test.js
@@ -0,0 +1,5 @@
+import { sum } from './index';
+
+test('test works', () => {
+  expect(sum(1, 1)).toBe(2);
+});
