f1ace123fa88d92893b73bbf21f3c9ab3356d856
gringrape <gringrape200@gmail.com>
2021-11-08 21:17:15 +0900

    프로그래머스 - 최소직사각형

--- 20211108/javascript/solution.test.js
+++ 20211108/javascript/solution.test.js
@@ -1,3 +1,16 @@
+function solution(sizes) {
+  const max = (arr) => Math.max(...arr);
+  const min = (arr) => Math.min(...arr);
+
+  const multiply = (a, b) => a * b;
+
+  return [max, min]
+    .map((mapper) => sizes.map(mapper))
+    .map(max)
+    .reduce(multiply);
+}
+
 test('test works', () => {
-  expect(1 + 1).toBe(2);
+  expect(solution([[60, 50], [30, 70], [60, 30], [80, 40]])).toBe(4000);
+  expect(solution([[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]])).toBe(120);
 });
