193cd7b968e3a37ac9a48ccfbdc373e0213833d4
gringrape <gringrape200@gmail.com>
2021-12-27 20:00:24 +0900

    Set up a new javascript project

---
+++ 20211227/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 13,
+  },
+  rules: {
+  },
+};
---
+++ 20211227/solution.test.js
@@ -0,0 +1,3 @@
+test('test work', () => {
+  expect(1 + 1).toBe(2);
+});
